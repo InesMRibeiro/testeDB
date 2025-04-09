10e86500443eac4503cd3d97934b74fa143f1a33
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-04-09 17:12:52 +0100

    aceita all hosts

--- backend/app.py
+++ backend/app.py
@@ -36,4 +36,5 @@ def get_names():
     return jsonify([name.name for name in names]), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
