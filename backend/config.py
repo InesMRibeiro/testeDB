bdce565f48893ed18c2e7de9bd2fec7264a6ffa5
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-04-09 17:00:10 +0100

    alteralão backend db name

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ines:040102@54.208.35.249:5432/Names')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://ines:040102@54.208.35.249:5432/names')
